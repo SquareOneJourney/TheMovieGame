8884d2587112220093e5ef0d8abd215679365611
MediocracyHD <bdavis241@outlook.com>
2025-10-13 23:58:44 -0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import './globals.css'
 import { Providers } from './providers'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Analytics } from '@vercel/analytics/react'
-import PWAInstaller from './components/PWAInstaller'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -116,7 +115,6 @@ export default function RootLayout({
         <Providers>
           {children}
         </Providers>
-        <PWAInstaller />
         <SpeedInsights />
         <Analytics />
         <script
